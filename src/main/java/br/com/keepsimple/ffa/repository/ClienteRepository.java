d7bf0274146f6130cd87574385a23410b1c16a6a
Sandro Vieira <slzvieira@gmail.com>
2018-04-29 21:21:45 -0300

    Exclusão de arquivos antigos.

--- src/main/java/br/com/keepsimple/ffa/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.keepsimple.ffa.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository { //extends JpaRepository<Cliente, Integer> {
-	
-}
